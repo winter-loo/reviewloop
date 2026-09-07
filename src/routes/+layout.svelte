005251678fc7da433020a75d6dd00022f8200366
winter.loo <adjkldd@126.com>
2026-09-08 03:28:56 +0800

    Draw a clean solid-color SVG favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.png';
+	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" type="image/png" href={favicon} />
+	<link rel="icon" type="image/svg+xml" href={favicon} />
 </svelte:head>
 
 {@render children()}
